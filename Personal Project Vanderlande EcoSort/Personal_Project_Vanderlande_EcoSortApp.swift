de4eadc2814616b388ee01531ecf1366f6f7abe9
Jasmin Hachmane <509304@student.fontys.nl>
2025-06-21 11:15:36 +0200

    Initial Commit

---
+++ Personal Project Vanderlande EcoSort/Personal_Project_Vanderlande_EcoSortApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_Project_Vanderlande_EcoSortApp.swift
+//  Personal Project Vanderlande EcoSort
+//
+//  Created by Jasmin Hachmane on 21/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Personal_Project_Vanderlande_EcoSortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
